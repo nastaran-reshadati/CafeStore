75c8c34e6002e89b161d318385402d4826d7efa5
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2024-01-26 22:27:46 +0330

    tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        "shadow-normal": "0 1 10px rgba(0, 0, 0, 0.5)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemibold: "Dana DemiBold",
+        Morabba: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+    },
+  },
+  plugins: [],
+};
